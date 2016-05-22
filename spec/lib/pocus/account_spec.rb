b948020789f5224f07b1f098ac01f5049358f750
Piers Chambers <piers@varyonic.com>
2016-05-21 20:50:26 -0400

    Add flag to turn on API logging in spec.

--- spec/lib/pocus/account_spec.rb
+++ spec/lib/pocus/account_spec.rb
@@ -6,6 +6,7 @@ include Pocus::Fixtures
 
 RSpec.describe Pocus::Account do
   Pocus::Session.config(fixtures(:credentials))
+  Pocus::Session.instance.logger = Logger.new(STDOUT) if ENV['POCUS_DEBUG']
   let(:account) { Pocus::Account.new(account_id: fixtures(:account_id)) }
   let(:test_folder) { account.clientfolders.find(fixtures(:test_client_folder_id)) }
   let(:folder_attributes) do
